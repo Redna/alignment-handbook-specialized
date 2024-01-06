b3d0741d51bd6a6f70f3538ae0299e2c541799f4
Alexander Gruhl <gruhl.alexander@gmail.com>
2024-01-07 00:25:02 +0100

    adjusted training script

--- runpodio_training.sh
+++ runpodio_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Your first command
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml --num_processes=1 scripts/run_sft.py recipes/tinyllama-1_1b_reasoning_v2_sft/dpo/config_full.yaml > output.log 2>&1
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml --num_processes=1 scripts/run_dpo.py recipes/tinyllama-1_1b_reasoning_v2_sft/dpo/config_full.yaml > output.log 2>&1
 
 # Your second command
 runpodctl remove pod $RUNPOD_POD_ID
